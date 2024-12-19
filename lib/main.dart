cdffba3d3d5be29a59b51a554dd892c9bc234c4a
tri <votri.31012003@gmail.com>
2024-12-19 23:26:49 +0700

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:laptrinhdidong/presentation/home_page/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MainApp());
 }
 
